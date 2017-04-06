3659796be2ff2738ecaf9c65f4104353753f5150
Guillaume Vial-Pailler <dev.guihub@gmail.com>
2017-04-06 16:20:14 +0100

    Adding logic outside of render function

--- App/Components/Game.js
+++ App/Components/Game.js
@@ -16,61 +16,64 @@ class Game extends Component{
       currentQuestion: this.props.questionSet[0],
       correctAnswer: this.props.questionSet[0].correct_answer,
       incorrectAnswers: this.props.questionSet[0].incorrect_answers,
-      allAnswers: [],
-      isUserAnswerCorrect: '',
+      userAnswer: null,
+      isUserAnswerCorrect: null,
+      allShuffledAnswers: [...this.props.questionSet[0].incorrect_answers,this.props.questionSet[0].correct_answer],
       score: 0,
     }
   }
-  shuffle(array){
-    for (let i = array.length-1; i >=0; i--) {
+
+  componentDidMount(){
+    let allShuffledAnswers = this.shuffle(this.state.allShuffledAnswers);
+
+    this.setState({
+      allShuffledAnswers: allShuffledAnswers
+    });
+  }
+
+  shuffle(questionAnswers){
+    for (let i = questionAnswers.length-1; i >=0; i--) {
       let randomIndex = Math.floor(Math.random()*(i+1));
-      let itemAtIndex = array[randomIndex];
+      let itemAtIndex = questionAnswers[randomIndex];
          
-      array[randomIndex] = array[i];
-      array[i] = itemAtIndex;
+      questionAnswers[randomIndex] = questionAnswers[i];
+      questionAnswers[i] = itemAtIndex;
     }
-    return array;
+    return questionAnswers
   }
-  handleResult(){
-    if (this.state.isUserAnswerCorrect == true){
-      let score = this.state.score
-      this.setState({
-        score: score + 10
-      })
-      console.log('change color of background answer')
-    } else {
-      console.log('change color of background answer')
-    }
-  }
+
   handleAnswer(answer){
     console.log('answer :', answer)
+    let isUserAnswerCorrect = this.state.isUserAnswerCorrect;
+    let score = this.state.score;
+    let userAnswer = this.state.userAnswer;
+
     if (answer == this.state.correctAnswer){
-      this.setState({
-        isUserAnswerCorrect: true,},
-        () => {
-          this.handleResult()
-      })
+      isUserAnswerCorrect = true;
+      score = score + 10;
+      userAnswer = answer
     }
     else {
-      this.setState({
-        isUserAnswerCorrect: false,},
-        () => {
-          this.handleResult()
-      })
+      isUserAnswerCorrect = false;
+      score = score
+      userAnswer = answer
+      console.log('change color of background answer')
+
     }
+
+    // shuufle
+
+    this.setState({
+      isUserAnswerCorrect: isUserAnswerCorrect,
+      score: score,
+      userAnswer: userAnswer,
+      //shuffled answers from next question
+    });
   }
   render(){
     let _this = this;
-    let allAnswers = [];
-    let correctAnswer = this.state.correctAnswer;
-    let incorrectAnswers = this.state.incorrectAnswers;
-    let currentQuestion = this.state.currentQuestion
-    console.log('currentQuestion: ', currentQuestion)
-    allAnswers.push(correctAnswer);
-    for (i of incorrectAnswers) {
-      allAnswers.push(i);
-    }
-    let allShuffledAnswers = this.shuffle(allAnswers)
+    let allShuffledAnswers = this.state.allShuffledAnswers;
+    console.log('currentQuestion: ', this.state.currentQuestion)
 
     return (
       <View style={styles.mainContainer}>
@@ -78,9 +81,13 @@ class Game extends Component{
         <Text style={styles.title}>Question: {this.state.currentQuestion.question}</Text>
         <View style={styles.answersDeck}>
           {
-            allShuffledAnswers.map(function (item) {
+            this.state.allShuffledAnswers.map((item) => {
+
+              const correctAnswerStyle = (this.state.isUserAnswerCorrect == true) && this.state.correctAnswer === item ? {backgroundColor:'green'} : null;
+              const incorrectAnswerStyle = (this.state.isUserAnswerCorrect == false) && this.state.correctAnswer != item ? {backgroundColor:'red'} : null;
+
               return (
-                <View key={item} style={styles.answer}>
+                <View key={item} style={[styles.answer,correctAnswerStyle,incorrectAnswerStyle]}>
                   <TouchableHighlight onPress={() => _this.handleAnswer(item)}>
                     <Text style={styles.answerText}> {item} </Text>
                   </TouchableHighlight>
